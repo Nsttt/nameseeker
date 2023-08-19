8271d032fc3899d372cc3aa000fe571e8a0c423f
Nestor <nestor@nstlopez.com>
2023-08-19 12:43:07 +0200

    Fix cases for exceptions

--- src/app/api/exists/github_org/route.ts
+++ src/app/api/exists/github_org/route.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
@@ -9,24 +9,30 @@ const octokit = new Octokit({
 });
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const name = searchParams.get("name");
+  try {
+    const { searchParams } = new URL(req.url);
+    const name = searchParams.get("name");
+    const response = await octokit.request(`GET /orgs/${name}`);
 
-  const response = await octokit.request(`GET /orgs/${name}`);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const exists = response?.data?.html_url as string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const exists = response?.data?.html_url as string | undefined;
-
-  return NextResponse.json(
-    exists
-      ? {
-          provider: "github_org",
-          _exists: "yes",
-          url: exists,
-        }
-      : {
-          provider: "github_org",
-          _exists: "no",
-        },
-  );
+    return NextResponse.json(
+      exists
+        ? {
+            provider: "github_org",
+            _exists: "yes",
+            url: exists,
+          }
+        : {
+            provider: "github_org",
+            _exists: "no",
+          },
+    );
+  } catch (error) {
+    return NextResponse.json({
+      provider: "github_org",
+      _exists: "no",
+    });
+  }
 }
--- src/app/api/exists/gitlab/route.ts
+++ src/app/api/exists/gitlab/route.ts
@@ -3,45 +3,52 @@ import "server-only";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const name = searchParams.get("name");
+  try {
+    const { searchParams } = new URL(request.url);
+    const name = searchParams.get("name");
 
-  const fetchPage = async (page: number) => {
-    const response = await fetch(
-      `https://gitlab.com/api/v4/projects?search="${name}"&per_page=100&page=${page}`,
-    );
-    return response.json();
-  };
+    const fetchPage = async (page: number) => {
+      const response = await fetch(
+        `https://gitlab.com/api/v4/projects?search="${name}"&per_page=100&page=${page}`,
+      );
+      return response.json();
+    };
 
-  const findUrlInPage = async (page: number): Promise<string | null> => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const json = await fetchPage(page);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (json.length === 0) {
-      throw new Error("Length of JSON is 0");
-    }
+    const findUrlInPage = async (page: number): Promise<string | null> => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const json = await fetchPage(page);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (json.length === 0) {
+        throw new Error("Length of JSON is 0");
+      }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
-    const url = json.find((value: any) => value.name === name)?.web_url;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+      const url = json.find((value: any) => value.name === name)?.web_url;
 
-    if (url) {
-      throw new Error("Issue finding url");
-    }
+      if (url) {
+        throw new Error("Issue finding url");
+      }
 
-    if (page < 10) {
-      return findUrlInPage(page + 1);
-    }
+      if (page < 10) {
+        return findUrlInPage(page + 1);
+      }
 
-    throw new Error("Issue in findUrlInPage");
-  };
+      throw new Error("Issue in findUrlInPage");
+    };
 
-  const foundUrl = await findUrlInPage(1);
+    const foundUrl = await findUrlInPage(1);
 
-  return foundUrl
-    ? NextResponse.json({
-        provider: "gitlab",
-        _exists: "yes",
-        url: foundUrl,
-      })
-    : NextResponse.json({ provider: "gitlab", _exists: "no" });
+    return foundUrl
+      ? NextResponse.json({
+          provider: "gitlab",
+          _exists: "yes",
+          url: foundUrl,
+        })
+      : NextResponse.json({ provider: "gitlab", _exists: "no" });
+  } catch (error) {
+    return NextResponse.json({
+      provider: "gitlab",
+      _exists: "no",
+    });
+  }
 }
