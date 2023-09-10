9cc9e826491d0375f31c6d24b7fbee6474e74f04
Nestor <nestor@nstlopez.com>
2023-09-10 22:53:22 +0200

    Add sourcemaps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   experimental: {
     serverActions: true,
     appDir: true,
